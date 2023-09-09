a0d2538b6a13a45f85e07dc5ac2306c6436d7143
dangakeri <dangakeri254@gmail.com>
2023-09-09 12:21:08 +0300

    implementing smooth scrolling

--- starter/script.js
+++ starter/script.js
@@ -29,7 +29,7 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
-
+/*
 // Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -106,3 +106,36 @@ logo.classList.add('c')
 logo.classList.remove('c')
 logo.classList.toggle('c')
 logo.classList.contains('c')
+*/
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.scrollX, scrollY);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // Old skool way
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth'
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
