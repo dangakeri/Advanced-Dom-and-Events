5ab5db84bb9c47f44e67cdba9c6d8e9873e08dcb
dangakeri <dangakeri254@gmail.com>
2023-09-09 11:35:13 +0300

    styles attributes and classes

--- starter/script.js
+++ starter/script.js
@@ -56,8 +56,53 @@ header.append(message);
 // header.after(message);
 // header.before(message);
 
-// Delete Elements 
+// Delete Elements
 document.querySelector('.btn--close-cookie').addEventListener('click', function () {
   // message.parentElement.removeChild(message);
   message.remove();
 })
+
+
+// Styles 
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.twitter-link');
+
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c')
+logo.classList.remove('c')
+logo.classList.toggle('c')
+logo.classList.contains('c')
