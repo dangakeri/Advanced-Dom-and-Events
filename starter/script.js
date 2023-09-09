f335d1f8dd27161bd6ec386645a3f52648adcdb2
dangakeri <dangakeri254@gmail.com>
2023-09-09 13:47:52 +0300

    Event propagation

--- starter/script.js
+++ starter/script.js
@@ -112,30 +112,69 @@ const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+  /*
+ const s1coords = section1.getBoundingClientRect();
+ console.log(s1coords);
 
-  console.log(e.target.getBoundingClientRect());
+ console.log(e.target.getBoundingClientRect());
 
-  console.log('Current scroll (X/Y)', window.scrollX, scrollY);
+ console.log('Current scroll (X/Y)', window.scrollX, scrollY);
 
-  console.log(
-    'height/width viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
-
-  // Scrolling
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //   s1coords.top + window.pageYOffset
-  // );
-
-  // Old skool way
-  // window.scrollTo({
-  //   left: s1coords.left + window.scrollX,
-  //   top: s1coords.top + window.scrollY,
-  //   behavior: 'smooth'
-  // });
+ console.log(
+   'height/width viewport',
+   document.documentElement.clientHeight,
+   document.documentElement.clientWidth,
+ );
+ 
+   // Scrolling
+   // window.scrollTo(
+   //   s1coords.left + window.pageXOffset,
+   //   s1coords.top + window.pageYOffset
+   // );
+ 
+   // Old skool way
+   // window.scrollTo({
+   //   left: s1coords.left + window.scrollX,
+   //   top: s1coords.top + window.scrollY,
+   //   behavior: 'smooth'
+   // });*/
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+// Types of events and Events Handlers
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+  // h1.removeEventListener('mouseenter', alertH1)
+}
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// } 
+
+// Event propagation
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+},
+);
